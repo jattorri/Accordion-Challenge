81dc70814ed1b4a5cb67c806d099c6c26a68b367
Juan Attorri <jattorri@gmail.com>
2021-08-21 12:36:31 -0300

    actualizando estilos

--- src/Cart.js
+++ src/Cart.js
@@ -4,13 +4,71 @@ import box from './images/illustration-box-desktop.svg';
 
 const Cart = () => {
   return (
-    <section>
-      <div className='cart-container'>
-        <div className='img-container'>
-          <img src={img} alt='img' />
+    <section className='card'>
+      <img className='img-cardbox' src={box} alt='box' />
+      <div className='img-container'>
+        <img src={img} alt='img' />
+      </div>
+      <div className='questions-container'>
+        <h1>FAQ</h1>
+        <div className='acordion-item'>
+          <h2>
+            <button className='acordion-question'>
+              Jelly sweet roll jelly
+            </button>
+          </h2>
+          <div className='acordion-answer'>
+            beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie
+            sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant
+            tootsie roll lollipop.
+          </div>
+        </div>
+        <div className='acordion-item'>
+          <h2>
+            <button className='acordion-question'>
+              Jelly sweet roll jelly
+            </button>
+          </h2>
+          <div className='acordion-answer'>
+            beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie
+            sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant
+            tootsie roll lollipop.
+          </div>
+        </div>
+        <div className='acordion-item'>
+          <h2>
+            <button className='acordion-question'>
+              Jelly sweet roll jelly
+            </button>
+          </h2>
+          <div className='acordion-answer'>
+            Marzipan marshmallow croissant tootsie roll lollipop.
+          </div>
+        </div>
+        <div className='acordion-item'>
+          <h2>
+            <button className='acordion-question'>
+              Jelly sweet roll jelly
+            </button>
+          </h2>
+          <div className='acordion-answer'>
+            beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie
+            sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant
+            tootsie roll lollipop.
+          </div>
+        </div>
+        <div className='acordion-item'>
+          <h2>
+            <button className='acordion-question'>
+              Jelly sweet roll jelly
+            </button>
+          </h2>
+          <div className='acordion-answer'>
+            beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie
+            croissant tootsie roll lollipop.
+          </div>
         </div>
       </div>
-      <img src={box} alt='box' className='box' />
     </section>
   );
 };
